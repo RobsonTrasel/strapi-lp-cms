c1d5d236fc49b5285fb70c81fceaae8b583ac632
RobsonTrasel <robson.trasel@gmail.com>
2022-11-12 23:56:41 -0300

    fix: change configs for enable cloudinary support

--- config/database.js
+++ config/database.js
@@ -2,12 +2,16 @@ module.exports = ({ env }) => ({
   connection: {
     client: 'postgres',
     connection: {
-      host: env('DATABASE_HOST', ''),
-      port: env.int('DATABASE_PORT', ),
-      database: env('DATABASE_NAME', ''),
-      user: env('DATABASE_USERNAME', ''),
+      host: env('DATABASE_HOST', 'localhost'),
+      port: env.int('DATABASE_PORT', 5432),
+      database: env('DATABASE_NAME', 'strapi-db'),
+      user: env('DATABASE_USERNAME', 'root'),
       password: env('DATABASE_PASSWORD', ''),
       ssl: env.bool('DATABASE_SSL', true),
     },
+    pool: {
+      min: 0,
+      max: 5
+    }
   },
 });
--- config/middlewares.js
+++ config/middlewares.js
@@ -1,6 +1,19 @@
 module.exports = [
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'https:'],
+          'img-src': ["'self'", 'data:', 'blob:', 'dl.airtable.com', 'res.cloudinary.com'],
+          'media-src': ["'self'", 'data:', 'blob:', 'dl.airtable.com', 'res.cloudinary.com'],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
   'strapi::errors',
-  'strapi::security',
   'strapi::cors',
   'strapi::poweredBy',
   'strapi::logger',

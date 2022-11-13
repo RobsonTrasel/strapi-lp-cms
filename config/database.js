fa295758149713aaeff0ee2d1c30a62d1771328c
RobsonTrasel <robson.trasel@gmail.com>
2022-11-12 23:33:27 -0300

    fix: change vulnerabilities

--- config/database.js
+++ config/database.js
@@ -2,11 +2,11 @@ module.exports = ({ env }) => ({
   connection: {
     client: 'postgres',
     connection: {
-      host: env('DATABASE_HOST', 'babar.db.elephantsql.com '),
-      port: env.int('DATABASE_PORT', 5432),
-      database: env('DATABASE_NAME', 'fxggmoge'),
-      user: env('DATABASE_USERNAME', 'fxggmoge'),
-      password: env('DATABASE_PASSWORD', 'eIWxDpfzW7xDQShC4ejN3audm9UyMLvZ'),
+      host: env('DATABASE_HOST', ''),
+      port: env.int('DATABASE_PORT', ),
+      database: env('DATABASE_NAME', ''),
+      user: env('DATABASE_USERNAME', ''),
+      password: env('DATABASE_PASSWORD', ''),
       ssl: env.bool('DATABASE_SSL', true),
     },
   },
